c8783f453dff7cdaef3c3820c573846bae662eb0
zgarceau <zgarceau@comcast.net>
2021-02-24 22:05:48 -0600

    deploy react app to github pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My Portfolio</h1>
+	<h2>Author: Zachary Garceau</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
